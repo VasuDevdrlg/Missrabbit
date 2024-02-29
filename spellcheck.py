3f27f72339b4a003161896647f2befbda2b5ecf1
DEVA <87009549+VasuDevdrlg@users.noreply.github.com>
2024-02-29 10:37:05 +0530

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -1,12 +1,1 @@
-from spellchecker import SpellChecker
-def bee(input):
-    root=SpellChecker()
-    list=[]
-    list.append(input)
-    token=""
-    wrong=root.unknown(list)
-    for i in wrong:
-      correct=root.candidates(i)
-    for j in correct:
-     token+="👉 "+j+"\n"
-    return token
+
